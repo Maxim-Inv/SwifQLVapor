a9df1c8c496783f4c07b4d46c096885f59aa277a
Maxim Anisimov <maxim@invoodoo.com>
2020-04-02 13:27:50 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         ],
         dependencies: [
         .package(url: "https://github.com/MihaelIsaev/SwifQL.git", from: "2.0.0-beta"),
-        .package(url: "https://github.com/MihaelIsaev/SwifQLNIO.git", from:"2.0.0"),
+        .package(url: "https://github.com/Maxim-Inv/SwifQLNIO.git", .branch("master"),
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-rc"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0-rc"),
         .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0-rc"),

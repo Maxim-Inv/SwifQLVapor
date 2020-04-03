10a54d94b4782a7decc9d9a83d66c84a3aa52309
Maxim Anisimov <maxim@invoodoo.com>
2020-04-03 21:25:59 +0300

    Fix for `postgres-nio` breaking changes

--- Sources/SwifQLVapor/SwifQLable+Execute.swift
+++ Sources/SwifQLVapor/SwifQLable+Execute.swift
@@ -19,7 +19,7 @@ extension SwifQLable {
             try prepared.values.map { try PostgresDataEncoder().encode($0) }
         }.flatMap { values in
             database.query(prepared.query, values)
-        }
+        }.map { $0.rows }
     }
 }
 
